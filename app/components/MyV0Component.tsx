497913eb763a9c68e0de59218c9e289fbd7c0b82
gabi-lacerda <148162437+gabi-lacerda@users.noreply.github.com>
2023-11-15 09:08:08 +1000

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,83 +1,52 @@
 /**
  * v0 by Vercel.
- * @see https://v0.dev/t/goAPqBgT53Z
+ * @see https://v0.dev/t/tzWGEJ7sWj1
  */
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
 export default function Component() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 bg-gradient-to-b from-white via-pink-300 to-pink-500">
-      <div className="container grid gap-6 lg:grid-cols-3 px-4 md:px-6">
-        <div className="flex flex-col items-center justify-center space-y-4 text-center transition-colors hover:bg-pink-300 rounded-lg bg-white bg-opacity-50 backdrop-blur-md shadow-xl">
-          <img
-            alt="Cloud background"
-            className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
-            height="200"
-            src="/placeholder.svg"
-            style={{
-              aspectRatio: "200/200",
-              objectFit: "cover",
-            }}
-            width="200"
-          />
-          <div className="bg-white bg-opacity-70 p-4 rounded-lg shadow-inner z-20">
-            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl text-pink-800">Basic</h2>
-            <div className="text-4xl font-bold text-pink-800">$9.99</div>
-            <p className="mx-auto max-w-[700px] text-pink-800 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              For individuals or small teams starting to manage tasks.
-            </p>
-            <Button className="bg-pink-700 hover:bg-pink-600" variant="default">
-              Select Plan
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-bold text-gray-900 dark:text-gray-200">
+            Login to your account
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6">
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <Label htmlFor="username">Username</Label>
+              <Input
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 dark:border-gray-700 placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-gray-100 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                id="username"
+                required
+                type="text"
+              />
+            </div>
+            <div>
+              <Label htmlFor="password">Password</Label>
+              <Input
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 dark:border-gray-700 placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-gray-100 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                id="password"
+                required
+                type="password"
+              />
+            </div>
+          </div>
+          <div>
+            <Button
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gradient-to-r from-red-500 via-yellow-500 to-purple-600 hover:from-red-600 hover:via-yellow-600 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+              type="submit"
+            >
+              Login
             </Button>
           </div>
-        </div>
-        <div className="flex flex-col items-center justify-center space-y-4 text-center transition-colors hover:bg-pink-300 rounded-lg bg-white bg-opacity-50 backdrop-blur-md shadow-xl">
-          <img
-            alt="Cloud background"
-            className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
-            height="200"
-            src="/placeholder.svg"
-            style={{
-              aspectRatio: "200/200",
-              objectFit: "cover",
-            }}
-            width="200"
-          />
-          <div className="bg-white bg-opacity-70 p-4 rounded-lg shadow-inner z-20">
-            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl text-pink-800">Pro</h2>
-            <div className="text-4xl font-bold text-pink-800">$19.99</div>
-            <p className="mx-auto max-w-[700px] text-pink-800 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              For growing teams that need to manage more work.
-            </p>
-            <Button className="bg-pink-700 hover:bg-pink-600" variant="default">
-              Select Plan
-            </Button>
-          </div>
-        </div>
-        <div className="flex flex-col items-center justify-center space-y-4 text-center transition-colors hover:bg-pink-300 rounded-lg bg-white bg-opacity-50 backdrop-blur-md shadow-xl">
-          <img
-            alt="Cloud background"
-            className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
-            height="200"
-            src="/placeholder.svg"
-            style={{
-              aspectRatio: "200/200",
-              objectFit: "cover",
-            }}
-            width="200"
-          />
-          <div className="bg-white bg-opacity-70 p-4 rounded-lg shadow-inner z-20">
-            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl text-pink-800">Enterprise</h2>
-            <div className="text-4xl font-bold text-pink-800">Custom</div>
-            <p className="mx-auto max-w-[700px] text-pink-800 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              For large teams with complex workflows and higher needs. Pricing can be customised to suit your needs.
-            </p>
-            <Button className="bg-pink-700 hover:bg-pink-600" variant="default">
-              Contact Us
-            </Button>
-          </div>
-        </div>
+        </form>
       </div>
-    </section>
+    </div>
   )
 }
+
